a1233345838e2624ab53bfda8eb199f831285d24
Chris Weber <chris.weber@kslzh.ch>
2023-07-16 12:17:30 +0200

    fix variable key length in RSA.generateKeyPair()

--- EncryptionPlayground.java
+++ EncryptionPlayground.java
@@ -13,12 +13,5 @@ public class EncryptionPlayground {
     public static void main(String[] args) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, 
             IllegalBlockSizeException, BadPaddingException, GeneralSecurityException, IOException, Exception {
 
-        // AES.generateKey(256);
-        // AES.encryptFile("ClearText/clear", "EncryptedFiles/encrypted", "SymmetricKey/secretKey");
-        // AES.decryptFile("EncryptedFiles/encrypted", "DecryptedFiles/decrypted", "SymmetricKey/secretKey");
-
-        // RSA.generateKeyPair(2048);
-        // RSA.encryptFile("ClearText/cleartext.txt", "EncryptedFiles/encrypted", "KeyPair/publicKey");
-        // RSA.decryptFile("EncryptedFiles/encrypted", "DecryptedFiles/decrypted", "KeyPair/privateKey");
     }
 }
